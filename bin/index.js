a9aa2f8bc5ef79aec9078d9b6a48dd1b568a6ccc
RGR <rgr@local>
2023-11-21 00:11:00 +0100

    chatgpt optimalisaties

--- bin/index.js
+++ bin/index.js
@@ -1,34 +1,29 @@
 #!/usr/bin/env node
 
+/*
+
+- Controle of printers bestaan - Indien niet, error
+- Testprintje sturen - bij error afkappen
+- Puppeteer starten
+- Websocket open
+
+*/
+
+
+
+
 require("dotenv").config();
 const chalk = require("chalk");
 const boxen = require("boxen");
-
 const { WebSocketServer } = require("ws");
 const SerialPort = require('serialport');
 const { print, getPrinters } = require("pdf-to-printer");
-const fs = require("fs");
+const fs = require("fs").promises;  // Gebruik de promisified versie van fs voor asynchrone operaties
 const { v4: uuidv4 } = require("uuid");
 const puppeteer = require("puppeteer");
 const { exec } = require('child_process');
 
 const wss = new WebSocketServer({ port: 3636 });
-
-wss.on("connection", function connection(ws) {
-  ws.on("error", console.error);
-
-  ws.on("message", async function message(data) {
-    console.log(data.toString('utf8'));
-
-    if (data.toString('utf8') === "openKassalade") {
-      openKassalade();
-    } else {
-      await verwerkTransactie(data, ws)
-    }
-  });
-
-  ws.send(JSON.stringify(true));
-});
 
 const greeting = chalk.white.bold(
   "Poort 3636 is er klaar voor!"
@@ -64,20 +59,36 @@ async function getBrowser() {
   return browserPromise;
 }
 
-getBrowser()
+getBrowser();
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", async function message(data) {
+    console.log(data.toString('utf8'));
+
+    if (data.toString('utf8') === "openKassalade") {
+      openKassalade();
+    } else {
+      await verwerkTransactie(data, ws);
+    }
+  });
+
+  ws.send(JSON.stringify(true));
+});
 
 async function verwerkTransactie(data, ws) {
   console.log("Zooi ontvangen");
   ws.send(JSON.stringify([0, "Transactie verwerken"]));
 
   try {
-    var file = await opslaanHTML(data);
+    const file = await opslaanHTML(data);
     ws.send(JSON.stringify([0.3, "Transactie verwerken"]));
     const browser = await getBrowser();
     await createPDF(file, ws, browser);
-    console.log("received: %s", file);
+    console.log("received:", file);
     ws.send(JSON.stringify([1, "Transactie verwerkt"]));
-    ws.send("OK")
+    ws.send("OK");
   } catch (error) {
     console.error("Fout bij verwerken van bericht:", error);
     // Hier kun je eventueel een foutreactie naar de client sturen.
@@ -95,51 +106,61 @@ function openKassalade() {
 }
 
 async function opslaanHTML(html) {
-  let uuid = uuidv4();
+  const uuid = uuidv4();
+  const filePath = `transacties/html/${uuid}.html`;
 
-  fs.writeFile("transacties/html/" + uuid + ".html", html, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("File written successfully\n");
-    }
-  });
+  try {
+    await fs.writeFile(filePath, html);
+    console.log("File written successfully\n");
+  } catch (err) {
+    console.error(err);
+  }
 
   return uuid;
 }
 
 async function createPDF(file, ws, browser) {
-  ws.send(JSON.stringify([0.4, "Transactie verwerken"]));
-  const page = await browser.newPage();
-  const html = fs.readFileSync("transacties/html/" + file + ".html", "utf-8");
-  await page.setContent(html, { waitUntil: "domcontentloaded" });
-  await page.emulateMediaType("print");
-  ws.send(JSON.stringify([0.6, "Transactie verwerken"]));
+  const htmlPath = `transacties/html/${file}.html`;
+  const pdfPath = `transacties/kassabonnen/${file}.pdf`;
 
-  const kassabon = await page.pdf({
-    path: "transacties/kassabonnen/" + file + ".pdf",
-    margin: { top: "10px", right: "5px", bottom: "0px", left: "5px" },
-    printBackground: true,
-    scale: 0.75,
-    width: "80mm",
-    height: "549mm",
-  });
+  try {
+    const html = await fs.readFile(htmlPath, "utf-8");
 
-  ws.send(JSON.stringify([0.7, "Kassabon printen"]));
-  await print("transacties/kassabonnen/" + file + ".pdf", {
-    printer: process.env.KASSABON_PRINTER
-  });
+    ws.send(JSON.stringify([0.4, "Transactie verwerken"]));
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: "domcontentloaded" });
+    await page.emulateMediaType("print");
+    ws.send(JSON.stringify([0.6, "Transactie verwerken"]));
 
-  ws.send(JSON.stringify([0.8, "Pakbon printen"]));
-  const pakbon = await page.pdf({
-    path: "transacties/pakbonnen/" + file + ".pdf",
-    margin: { top: "100px", right: "10px", bottom: "100px", left: "10px" },
-    printBackground: true,
-    format: "A4",
-  });
+    const kassabon = await page.pdf({
+      path: pdfPath,
+      margin: { top: "10px", right: "5px", bottom: "0px", left: "5px" },
+      printBackground: true,
+      scale: 0.75,
+      width: "80mm",
+      height: "549mm",
+    });
 
-  ws.send(JSON.stringify([0.9, "Pakbon printen"]));
-  await print("transacties/pakbonnen/" + file + ".pdf", { printer: process.env.PAKBON_PRINTER})
+    ws.send(JSON.stringify([0.7, "Kassabon printen"]));
+    await print(pdfPath, {
+      printer: process.env.KASSABON_PRINTER,
+    });
 
-  await page.close();
+    ws.send(JSON.stringify([0.8, "Pakbon printen"]));
+    const pakbonPath = `transacties/pakbonnen/${file}.pdf`;
+    const pakbon = await page.pdf({
+      path: pakbonPath,
+      margin: { top: "100px", right: "10px", bottom: "100px", left: "10px" },
+      printBackground: true,
+      format: "A4",
+    });
+
+    ws.send(JSON.stringify([0.9, "Pakbon printen"]));
+    await print(pakbonPath, { printer: process.env.PAKBON_PRINTER });
+
+    await page.close();
+  } catch (error) {
+    console.error("Fout bij verwerken van PDF:", error);
+    // Hier kun je eventueel een foutreactie naar de client sturen.
+  }
 }

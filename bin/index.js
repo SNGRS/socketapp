7ffb3802eed88ce22b41e78261f849976a3e41da
RGR <rgr@local>
2023-11-21 22:46:51 +0100

    optimalisaties voor console

--- bin/index.js
+++ bin/index.js
@@ -9,6 +9,7 @@ const fs = require("fs").promises;  // Gebruik de promisified versie van fs voor
 const { v4: uuidv4 } = require("uuid");
 const puppeteer = require("puppeteer");
 const { exec } = require('child_process');
+let browserPromise;
 
 const wss = new WebSocketServer({ port: 3636 });
 
@@ -16,23 +17,25 @@ wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
   ws.on("message", async function message(data) {
-    console.log(data.toString('utf8'));
+    //console.log(data.toString('utf8'));
+    console.log("\n")
 
     if (data.toString('utf8') === "openKassalade") {
       openKassalade();
     } else {
       await verwerkTransactie(data, ws);
     }
+
+    
   });
 
   ws.send(JSON.stringify(true));
 });
 
-let browserPromise;
-
 async function startBrowser() {
   const browser = await puppeteer.launch({ headless: "new" });
-  console.log("Puppeteer ook gestart");
+  //console.log("Puppeteer ook gestart");
+  console.log( chalk.bgGreen("BROWSER") + "\t Achtergrondhelper gestart" )
   return browser;
 }
 
@@ -40,11 +43,13 @@ async function getBrowser() {
   if (!browserPromise) {
     browserPromise = startBrowser();
   }
+
   return browserPromise;
 }
 
 async function verwerkTransactie(data, ws) {
-  console.log("Zooi ontvangen");
+  //console.log("Zooi ontvangen");
+  console.log( chalk.bgWhite.black("TRANSACTIE") + "\t Transactie ontvangen" )
   ws.send(JSON.stringify([0, "Transactie verwerken"]));
 
   try {
@@ -52,11 +57,12 @@ async function verwerkTransactie(data, ws) {
     ws.send(JSON.stringify([0.3, "Transactie verwerken"]));
     const browser = await getBrowser();
     await createPDF(file, ws, browser);
-    console.log("received:", file);
+    //console.log("received:", file);
     ws.send(JSON.stringify([1, "Transactie verwerkt"]));
     ws.send("OK");
   } catch (error) {
-    console.error("Fout bij verwerken van bericht:", error);
+    //console.error("Fout bij verwerken van bericht:", error);
+    console.log( chalk.bgRed("TRANSACTIE") + "\t Fout bij verwerken van bericht:" + error )
     // Hier kun je eventueel een foutreactie naar de client sturen.
   }
 }
@@ -64,10 +70,12 @@ async function verwerkTransactie(data, ws) {
 function openKassalade() {
   exec('OpenLade.exe', (error, stdout, stderr) => {
     if (error) {
-      console.error(`OpenLade.exe: ${error}`);
+      //console.error(`OpenLade.exe: ${error}`);
+      console.log( chalk.bgRed("KASSALADE") + `\t ${error}` )
       return;
     }
-    console.log('OpenLade.exe is gestart');
+    //console.log('OpenLade.exe is gestart');
+    console.log( chalk.bgWhite.black("KASSALADE") + "\t Kassalade geopend" )
   });
 }
 
@@ -77,9 +85,11 @@ async function opslaanHTML(html) {
 
   try {
     await fs.writeFile(filePath, html);
-    console.log("File written successfully\n");
+    //console.log("File written successfully\n");
+    console.log( chalk.bgWhite.black("TRANSACTIE") + "\t Transactie opgeslagen als " + uuid )
   } catch (err) {
-    console.error(err);
+    //console.error(err);
+    console.log( chalk.bgRed("TRANSACTIE") + "\t Fout bij opslaan van transactie: " + err )
   }
 
   return uuid;
@@ -88,16 +98,19 @@ async function opslaanHTML(html) {
 async function createPDF(file, ws, browser) {
   const htmlPath = `transacties/html/${file}.html`;
   const pdfPath = `transacties/kassabonnen/${file}.pdf`;
+  const pakbonPath = `transacties/pakbonnen/${file}.pdf`;
 
   try {
     const html = await fs.readFile(htmlPath, "utf-8");
 
     ws.send(JSON.stringify([0.4, "Transactie verwerken"]));
+    console.log( chalk.bgWhite.black("TRANSACTIE") + "\t PDF-maker geopend" )
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "domcontentloaded" });
     await page.emulateMediaType("print");
     ws.send(JSON.stringify([0.6, "Transactie verwerken"]));
 
+    console.log( chalk.bgWhite.black("TRANSACTIE") + "\t Kassabon maken" )
     const kassabon = await page.pdf({
       path: pdfPath,
       margin: { top: "10px", right: "5px", bottom: "0px", left: "5px" },
@@ -108,12 +121,15 @@ async function createPDF(file, ws, browser) {
     });
 
     ws.send(JSON.stringify([0.7, "Kassabon printen"]));
+    console.log( chalk.bgWhite.black("TRANSACTIE") + "\t Kassabon printen" )
     await print(pdfPath, {
       printer: process.env.KASSABON_PRINTER,
     });
+    console.log( chalk.bgWhite.black("TRANSACTIE") + "\t Kassabon geprint" )
 
     ws.send(JSON.stringify([0.8, "Pakbon printen"]));
-    const pakbonPath = `transacties/pakbonnen/${file}.pdf`;
+    
+    console.log( chalk.bgWhite.black("TRANSACTIE") + "\t Pakbon maken" )
     const pakbon = await page.pdf({
       path: pakbonPath,
       margin: { top: "100px", right: "10px", bottom: "100px", left: "10px" },
@@ -122,11 +138,13 @@ async function createPDF(file, ws, browser) {
     });
 
     ws.send(JSON.stringify([0.9, "Pakbon printen"]));
+    console.log( chalk.bgWhite.black("TRANSACTIE") + "\t Pakbon printen" )
     await print(pakbonPath, { printer: process.env.PAKBON_PRINTER });
+    console.log( chalk.bgWhite.black("TRANSACTIE") + "\t Pakbon geprint" )
 
     await page.close();
   } catch (error) {
-    console.error("Fout bij verwerken van PDF:", error);
+    console.log( chalk.bgRed("TRANSACTIE") + "\t Fout bij maken/printen PDF: " + err )
     // Hier kun je eventueel een foutreactie naar de client sturen.
   }
 }
@@ -156,36 +174,30 @@ async function checkAndPrint() {
     await print("startup.pdf", {
       printer: process.env.KASSABON_PRINTER,
     });
+    console.log( chalk.bgGreen("PRINTER") + "\t Kassabonprinter gestart" )
 
     await print("startup.pdf", {
       printer: process.env.PAKBON_PRINTER,
     });
+    console.log( chalk.bgGreen("PRINTER") + "\t Pakbonprinter gestart" )
 
-    console.log('Beide printers bestaan en printfuncties zijn succesvol uitgevoerd.');
   } catch (error) {
-    console.error(error.message);
+    console.log( boxen(error.message + "\nDit programma is afgesloten. Pas de instellingen opnieuw aan in '.env' en start dit programma opnieuw", {title: 'PRINTER \t FOUTMELDING', titleAlignment: 'center', backgroundColor: 'red', borderColor: 'red', padding: 2}) );
     process.exit(1); // Stop het script met een foutstatus
   }
 }
 
 async function run() {
+  console.log( boxen(chalk.white.bold('SOCKETAPP'), {padding: 1, borderStyle: 'double', width: 100, textAlignment: "center"}) )
+  console.log("Console wordt opgestart. Wacht totdat alles is opgestart")
+  console.log("-----------------------------------")
+  console.log( chalk.bgGreen("SOCKET") + "\t Websocket-server opgestart" )
+
   await checkAndPrint();
   await getBrowser();
 
-  const greeting = chalk.white.bold(
-    "Poort 3636 is er klaar voor!"
-  );
-  
-  const boxenOptions = {
-    padding: 1,
-    margin: 1,
-    borderStyle: "round",
-    borderColor: "green",
-    backgroundColor: "#555555",
-  };
-  const msgBox = boxen(greeting, boxenOptions);
-  
-  console.log(msgBox);
+  console.log( boxen("App is opgestart. Goede verkoop!", { backgroundColor: 'green', padding: {top: 1, bottom: 1} }) )
+  console.log("-----------------------------------")
 }
 
 // Start de uitvoering
